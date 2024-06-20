33a7427715770dcf27cc78cd2ccc04ea28b7ef32
082_Farrel Emerson <FarrelEmerson02@gmail.com>
2024-06-20 20:34:33 +0700

    Updated

--- FaceRegUser/app/src/main/java/com/example/facereguser/server/serverAPI.java
+++ FaceRegUser/app/src/main/java/com/example/facereguser/server/serverAPI.java
@@ -12,7 +12,7 @@ public class serverAPI {
     //    public final static String BASE_URL = "http://etc.if.its.ac.id/";
 //    private final static String API_BASE_URL = BASE_URL+"api/v1/";
     //public final static String BASE_URL = "http://192.168.0.110/";
-    public final static String BASE_URL = "https://13b2-103-94-190-18.ngrok-free.app";
+    public final static String BASE_URL = "https://4171-103-94-190-27.ngrok-free.app";
 
     public static Retrofit builder() {
         HttpLoggingInterceptor interceptor = new HttpLoggingInterceptor();
